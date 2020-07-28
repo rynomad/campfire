abd0e5829a88b3bf2810f56d520caf362b2ac8af
Ryan Bennett <ry.nomad@protonmail.com>
2020-07-27 21:29:40 -0600

    fix layout

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,67 @@
+import React, { Component } from 'react'
 import { isMobile } from 'react-device-detect'
 
-export default function Home() {
-    return (
-        <div 
-            className="container"
-            style={{
-                position: 'fixed',
-                bottom: '0',
-                top: '0',
-                left: '0',
-                right: '0',
-                display: 'flex',
-                flexWrap: 'wrap-reverse'
-            }}
-            >
-            <div style={{
-                flexGrow: 1,
-                height : isMobile ? '30%' : '100%'
-            }}>
-            <iframe
-                class="airtable-embed"
-                src="https://airtable.com/embed/shrIc9Wxe7GnRs3Hp?backgroundColor=gray&viewControls=off"
-                frameborder="0"
-                onmousewheel=""
-                style={{
-                    width : '100%',
-                    height: '100%'
-                }}
-            />
-            </div>
-            <div style={{
-                flexGrow: 4,
-                height : isMobile ? '70%' : '100%'
-            }}>
-            <iframe
-                class="airtable-embed"
-                src="https://airtable.com/embed/shrkkkgpxxbC8H5Ru?backgroundColor=blue"
-                frameborder="0"
-                onmousewheel=""
-                style={{
-                    width: '100%',
-                    height: '100%'
-                }}
-            />
-            </div>
+class Home extends Component {
+    constructor(){
+        super()
+        this.state = {}
+    }
 
-        </div>
-    )
+    componentDidMount(){
+        this.setState({
+            isMobile
+        })
+    }
+
+    render(){
+        return (
+            <div 
+                className="container"
+                style={{
+                    position: 'fixed',
+                    bottom: '0',
+                    top: '0',
+                    left: '0',
+                    right: '0',
+                    display: 'flex',
+                    flexWrap: 'wrap-reverse'
+                }}
+                >
+                <div style={{
+                    flexGrow: 1,
+                    height : this.state.isMobile ? '30%' : '100%'
+                }}>
+                <iframe
+                    class="airtable-embed"
+                    src="https://airtable.com/embed/shrIc9Wxe7GnRs3Hp?backgroundColor=gray&viewControls=off"
+                    frameborder="0"
+                    onmousewheel=""
+                    style={{
+                        width : '100%',
+                        height: '100%'
+                    }}
+                />
+                </div>
+                <div style={{
+                    flexGrow: 4,
+                    height : this.state.isMobile ? '70%' : '100%'
+                }}>
+                <iframe
+                    class="airtable-embed"
+                    src="https://airtable.com/embed/shrkkkgpxxbC8H5Ru?backgroundColor=blue"
+                    frameborder="0"
+                    onmousewheel=""
+                    style={{
+                        width: '100%',
+                        height: '100%'
+                    }}
+                />
+                </div>
+    
+            </div>
+        )
+    }
+   
 }
+
+export default Home
