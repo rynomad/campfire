db0af3e3699222905562d4df8c01cd083158b90a
Ryan Bennett <ry.nomad@protonmail.com>
2020-07-28 02:58:29 -0600

    format

--- pages/index.js
+++ pages/index.js
@@ -2,93 +2,89 @@ import React, { Component } from 'react'
 import { isMobile, isTablet } from 'react-device-detect'
 
 class Home extends Component {
-    constructor(){
+    constructor() {
         super()
         this.state = {}
     }
 
-    componentDidMount(){
-        console.log('orientation', this.props.isPortrait, this.props.isLandscape)
+    componentDidMount() {
 
-        setTimeout(() => {
-            const watcher = window.matchMedia('(orientation: landscape)')
+        const watcher = window.matchMedia('(orientation: landscape)')
 
-            this.setState({
-                stack: (isMobile || isTablet) && !watcher.matches
-            })
-    
-            watcher.addListener((w) => {
-                const isLandscape = w.matches
-    
-                if (isLandscape && this.state.stack){
-                    this.setState({
-                        stack: false,
-                    })
-                } else if (!isLandscape && !this.state.stack){
-                    this.setState({
-                        stack: true
-                    })
-                }
-            })
+        this.setState({
+            stack: (isMobile || isTablet) && !watcher.matches
         })
 
+        watcher.addListener((w) => {
+            const isLandscape = w.matches
+
+            if (isLandscape && this.state.stack) {
+                this.setState({
+                    stack: false,
+                })
+            } else if (!isLandscape && !this.state.stack) {
+                this.setState({
+                    stack: true
+                })
+            }
+        })
     }
 
-    render(){
+    render() {
         return (
             <>
-            {/* <p>
+                {/* <p>
                 Overlay {JSON.stringify(this.state)} 
             </p> */}
-            <div 
-                className="container"
-                style={{
-                    position: 'fixed',
-                    bottom: '0',
-                    top: '0',
-                    left: '0',
-                    right: '0',
-                    display: 'flex',
-                    flexWrap: 'wrap-reverse',
-                    zIndex: -1
-                }}
+                <div
+                    className="container"
+                    style={{
+                        position: 'fixed',
+                        bottom: '0',
+                        top: '0',
+                        left: '0',
+                        right: '0',
+                        display: 'flex',
+                        flexWrap: 'wrap-reverse',
+                        zIndex: -1
+                    }}
                 >
-                <div style={{
-                    flexGrow: 1,
-                    height : this.state.stack ? '30%' : '100%'
-                }}>
-                <iframe
-                    class="airtable-embed"
-                    src="https://airtable.com/embed/shrIc9Wxe7GnRs3Hp?backgroundColor=gray&viewControls=off"
-                    frameborder="0"
-                    onmousewheel=""
-                    style={{
-                        width : '100%',
-                        height: '100%'
-                    }}
-                />
+                    <div style={{
+                        flexGrow: 1,
+                        height: this.state.stack ? '30%' : '100%'
+                    }}>
+                        <iframe
+                            class="airtable-embed"
+                            src="https://airtable.com/embed/shrIc9Wxe7GnRs3Hp?backgroundColor=gray&viewControls=off"
+                            frameborder="0"
+                            onmousewheel=""
+                            style={{
+                                width: '100%',
+                                height: '100%'
+                            }}
+                        />
+                    </div>
+                    <div style={{
+                        flexGrow: 4,
+                        height: this.state.stack ? '70%' : '100%'
+                    }}>
+                        <iframe
+                            class="airtable-embed"
+                            src="https://airtable.com/embed/shrkkkgpxxbC8H5Ru?backgroundColor=blue"
+                            frameborder="0"
+                            onmousewheel=""
+                            style={{
+                                width: '100%',
+                                height: '100%'
+                            }}
+                        />
+                    </div>
+
                 </div>
-                <div style={{
-                    flexGrow: 4,
-                    height : this.state.stack ? '70%' : '100%'
-                }}>
-                <iframe
-                    class="airtable-embed"
-                    src="https://airtable.com/embed/shrkkkgpxxbC8H5Ru?backgroundColor=blue"
-                    frameborder="0"
-                    onmousewheel=""
-                    style={{
-                        width: '100%',
-                        height: '100%'
-                    }}
-                />
-                </div>
-    
-            </div>
             </>
         )
     }
-   
+
 }
 
 export default Home
