5c5b2e7bb10d8c3d3699f6fa7cf661caa2a5572c
Ryan Bennett <ry.nomad@protonmail.com>
2020-08-05 03:17:13 -0600

    add moment

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
 import GalleryForm from './components/gallery_form'
+import moment from 'moment'
+
+const live = moment("Wed Aug 05 2020 18:50:00 GMT-0600 (Mountain Daylight Time)")
+const democracy = 'https://live.remesh.chat/p/99ea20b8-2a8e-451d-afc3-5371821dbc9e'
+const embed = false;
 
 class Home extends Component {
+    componentDidMount(){
+        if (moment().isAfter(live) && !embed){
+            window.location = democracy
+        }
+    }
+
     render() {
         return (
             <div
@@ -17,10 +28,16 @@ class Home extends Component {
                     top: '0'
                 }}
             >
-                <GalleryForm
+                {moment().isBefore(live)? (
+                    <h1 style={{
+                        color: 'white',
+                        textAlign: 'center',
+                        fontSize: 'xxx-large'
+                    }}> Coming Soon </h1>
+                ) : (<GalleryForm
                     gallery_url={"https://www.youtube.com/embed/BXPVe-YHDzo"}
-                    form_url={'https://live.remesh.chat/p/99ea20b8-2a8e-451d-afc3-5371821dbc9e'}
-                />
+                    form_url={democracy}
+                />)}
             </div>
         )
     }
