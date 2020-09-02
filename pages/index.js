87dd3c9fe40caf1da228a17896021b057dbfbf9f
Ryan Bennett <ry.nomad@protonmail.com>
2020-09-01 19:30:25 -0600

    update for wednesday

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,10 @@ import moment from 'moment'
 import YouTubeEmbed from '../src/youtube_embed'
 
 
-const live = moment("2020-08-31T18:00:00-0700")
+const live = moment("2020-09-02T18:00:00-0700")
 const preLive = live.clone().subtract(10, 'minutes')
 const smoreLive = preLive.clone().add(30, 'minutes')
-const youtube = "https://www.youtube.com/embed/GOwy20VioIg"
+const youtube = "https://www.youtube.com/embed/I3c7tHBGW7E"
 const democracy = "https://live.remesh.chat/p/7459a1fd-423e-43aa-8437-0068511d4ec8/"
 const smores = 'https://zoom.us/j/98695814291?pwd=d3Vpa0lDUU1kUDIwN3c1akJUL2VHQT09'
 
